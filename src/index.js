feeb02d314dc3b491c8ff48c6d523291aa69af8d
Sarankan Srikaran <sarankansrikaran@gmail.com>
2024-09-26 09:09:11 -0400

    Create a Todolist instnace and pass it to UI object
    Create Todolist instance in index , so it can be used to store all projects and tasks.
    Pass to UI, so UI can add projects to the Todolist object.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./styles.css";
 import homePage from "./home";
-
+import UI from './UI';
+import ToDoList from './todolist';
 
 homePage();
-
+const myToDoList = new ToDoList();
+const ui = new UI(myToDoList);  // Create instance of UI
+ui.addProjectBtns();  // Call instance method
+ui.addTasksBtns();
