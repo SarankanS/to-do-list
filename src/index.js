06927e27584e157bbb7e069ff9ae45b6213049bf
Sarankan Srikaran <sarankansrikaran@gmail.com>
2024-09-29 18:28:46 -0400

    Remove imports that weren't utilized

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import "./styles.css";
 import homePage from "./home";
 import UI from './UI';
-import ToDoList from './todolist';
 
 homePage();
-const myToDoList = new ToDoList();
-const ui = new UI(myToDoList);  // Create instance of UI
-ui.addProjectBtns();  // Call instance method
+const ui = new UI();  
+ui.addProjectBtns();  
 ui.addTasksBtns();
